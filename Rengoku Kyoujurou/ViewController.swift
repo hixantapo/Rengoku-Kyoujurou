f08a7847fb2b9996db2ab3f84dca87c7605b74b2
Boting Wang <hixantapo@gmail.com>
2020-11-08 18:07:23 +0800

    version 1.0

--- Rengoku Kyoujurou/ViewController.swift
+++ Rengoku Kyoujurou/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myAnimateGif: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myAnimateGif.loadGif(name: "Rengoku Kyoujurou Fight")
     }
-
 
 }
 
